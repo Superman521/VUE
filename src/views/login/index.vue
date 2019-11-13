43ae5469109e829e7968e24750ce386983bcfed2
Superman521 <877135756@qq.com>
2019-11-13 11:51:42 +0800

    黑马登录模块数据绑定

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -6,32 +6,78 @@
         <img src="../../assets/img/logo_index.png" alt />
       </div>
       <!-- 表单 -->
-      <el-form style="margin-top:30px">
+      <el-form :model="loginForm" :rules="loginRules" ref="formobj" style="margin-top:30px">
         <!-- 一个表单就是一个from -->
-        <el-form-item>
+        <el-form-item prop="mobile">
           <!-- 放置表单组件 -->
-          <el-input></el-input>
+          <el-input v-model="loginForm.mobile" placeholder="请输入您得手机号"></el-input>
         </el-form-item>
-        <el-form-item>
-          <el-input style="width:305px"></el-input>
+        <el-form-item prop="code">
+          <el-input style="width:305px" v-model="loginForm.code" placeholder="请输入验证码"></el-input>
           <el-button style="float:right">发送验证码</el-button>
         </el-form-item>
-        <el-form-item>
-          <el-checkbox>我已阅读并同意用户协议及条款</el-checkbox>
+        <el-form-item prop="checked">
+          <el-checkbox v-model="loginForm.checked" style="float:left">我已阅读并同意用户协议及条款</el-checkbox>
         </el-form-item>
         <el-form-item>
-          <el-button style="width:100%" type="primary">登录</el-button>
+          <el-button style="width:100%" type="primary" @click="login">登录</el-button>
         </el-form-item>
       </el-form>
     </el-card>
+    <div ref="abc"></div>
   </div>
 </template>
 
 <script>
+
 export default {
-  name: 'home',
-  components: {}
+  data () {
+    return {
+      // 要校验得整个表单数据
+      loginForm: {
+        mobile: '', // 手机号
+        code: '', // 验证码
+        checked: false
+      },
+      //   表单得验证规则
+      loginRules: {
+        // key(要校验得字段名).value(数组=>多条或者1条或者没有规则)
+        mobile:
+     [ { required: true, message: '请输入您得手机号' },
+       { pattern: /^1[3456789]\d{9}%/, message: '请输入正确得手机号' }
+     ],
+        code: [{ required: true, massage: '请输入验证码' }, {
+          pattern: /^\d{6}$/, massage: '请输入六位数字'
+        }],
+        checked: [{
+        // rule代表当前得规则
+        // value代表当前得值
+        // callback
+          validator: function (rule, value, callback) {
+            if (value) {
+              callback()
+            } else {
+              callback(new Error('请您阅读协议'))
+            }
+          }
+        }]
+      }
+    }
+  },
+  methods: {
+    login () {
+    //   console.log(this.$ref.abc)
+    //   this.$refs.formobj 获取el-form得对象实例
+      this.$refs.formobj.validate(function (isok) {
+        if (isok) {
+          //   如果为turn则去服务器核对调用接口登录
+        }
+      })
+    }
+  }
+
 }
+
 </script>
 <style lang="less" scoped>
 .login {
